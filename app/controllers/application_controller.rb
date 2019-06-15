0a0d7a4b6dc586d72b7e26ad021fe66b8a22d4a1
anupkhadka <akhadka.dev@gmail.com>
2019-06-15 17:33:12 -0400

    The index action makes an API request to coinmarketcap to get info on top 100 cryptocurrencies. Stores the data in an instance variable so that it can be accessed in view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
 
   def index
+    begin
+      @resp = Faraday.get 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' do |req|
+        req.headers['X-CMC_PRO_API_KEY'] = ENV['CMC_API_KEY']
+      end
+      resp_body = JSON.parse(@resp.body)
+      @crypto_data = resp_body["data"]
+    rescue Faraday::ConnectionFailed
+      @error = "There was an error accessing CoinMarketCap's API. Please try again."
+    end
   end
-  
+
 end
